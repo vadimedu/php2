a1aefacf8227506ac5b1de828e7465670551c74e
vadimedu <94390303+vadimedu@users.noreply.github.com>
2023-02-07 21:17:20 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,8 +3,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use GeekBrains\LevelTwo\User\User;
 use GeekBrains\LevelTwo\Blog\Posts;
+use GeekBrains\LevelTwo\Blog\Comments;
 use GeekBrains\LevelTwo\Repositories\SqliteUsersRepository;
 use GeekBrains\LevelTwo\Repositories\SqlitePostsRepository;
+use GeekBrains\LevelTwo\Repositories\SqliteCommentsRepository;
 
 $faker = Faker\Factory::create('ru_RU');
 
@@ -23,7 +25,21 @@ $connection = new PDO('sqlite:' . __DIR__ . '/blog.sqlite');
 
 $repo = new SqliteUsersRepository();
 $repo->save($di, $connection);
-$repoPost = new SqlitePostsRepository();
-$repoPost->save1($postObject, $connection);
-$repoPost->getPost($di->getId(), $connection);
+// $repoPost = new SqlitePostsRepository();
+// $repoPost->save1($postObject, $connection);
+// $repoPost->getPost($di->getId(), $connection);
+$commentObj = new Comments();
+$commentObj->setText('Комментарий к этой статье');
+$commentObj->setAuthorId($di->getId());
+$commentObj->setPostId(5);
+$repoComments = new SqliteCommentsRepository();
+$repoComments->saveComment($commentObj, $connection);
+if($_SERVER['REQUEST_URI'] && $_POST){
+    // var_dump($_POST);
+    // var_dump($_SERVER['REQUEST_URI']);
+    $newPostObject = $_POST;
+    // var_dump($newPostObject);
+   $saveNewPost = new SqlitePostsRepository();
+   $saveNewPost->saveFromCurl($newPostObject, $connection);
+}
 ?>
--- src/Repositories/SqlitePostsRepository.php
+++ src/Repositories/SqlitePostsRepository.php
@@ -13,7 +13,7 @@ class SqlitePostsRepository
     // public function __construct($connection){
     //     $this->connection = $connection;
     // }
-    public function save1(Posts $post, $connection)
+    public function save1($post, $connection)
     {
         $this->connection1 = $connection;
 
@@ -39,6 +39,30 @@ class SqlitePostsRepository
         var_dump($statement1);
         $result = $statement1->fetch(PDO::FETCH_ASSOC);
         var_dump($result);
+    }
+
+    public function saveFromCurl($post, $connection)
+    {
+        $this->connection1 = $connection;
+
+        var_dump($this->connection1);
+        var_dump($post);
+        $value = array_values($post);
+        var_dump($value[0]);
+        var_dump($value[1]);
+        var_dump($value[2]);
+        $statement1 = $this->connection1->prepare(
+
+            'INSERT INTO posts (id_author, title, content)
+            VALUES (:id_author, :title, :content)'
+            );
+
+            $statement1->execute([
+            ':title' => $value[1],
+            ':content' => $value[2],
+            ':id_author' => $value[0],
+            ]);
+
     }
 
 }
